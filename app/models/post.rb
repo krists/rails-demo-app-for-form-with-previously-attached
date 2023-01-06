4e40031fa43b4764d9e6a43a7e09d3275ecd928c
Krists Ozols <krists.ozols@gmail.com>
2023-01-06 22:03:02 +0200

    Display previously existing or unsaved attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
-  has_one_attached :main_image
-  has_many_attached :additional_images
+  has_one_attached :main_image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+  has_many_attached :additional_images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
   validates :title, presence: true
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4/app/assets/javascripts/activestorage.esm.js"
